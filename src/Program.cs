9973c8546fffc7544ef9d8f03160a7ca1def6f89
Artavazd Balaian <reasyu@gmail.com>
2018-08-04 15:16:28 +0700

    copy/paste

--- src/Program.cs
+++ src/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace teachatagoda
+namespace opensourceci
 {
     public class Program
     {
--- src/Startup.cs
+++ src/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace teachatagoda
+namespace opensourceci
 {
     public class Startup
     {
